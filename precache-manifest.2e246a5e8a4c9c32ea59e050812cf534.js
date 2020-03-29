97142438d136ebfb4e7286e0f6c85bd854c342a9
Brian Morgan <brian.s.morgan@gmail.com>
2020-03-29 01:22:26 +0000

    Updates

---
+++ precache-manifest.2e246a5e8a4c9c32ea59e050812cf534.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7dd60f40040a73a77799d1a3b1534d",
+    "url": "/konnected/index.html"
+  },
+  {
+    "revision": "2c29d49b0d1dd5f09de3",
+    "url": "/konnected/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1fe68d461dab9bdef04f",
+    "url": "/konnected/static/js/2.04cea63c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/konnected/static/js/2.04cea63c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c29d49b0d1dd5f09de3",
+    "url": "/konnected/static/js/main.ba99d9ef.chunk.js"
+  },
+  {
+    "revision": "0592da6d9c7d42c9c2c0",
+    "url": "/konnected/static/js/runtime-main.6d5723c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/konnected/static/media/logo.5d5d9eef.svg"
+  }
+]);
